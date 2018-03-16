fd4eb15197a0813bd9004c69aaf1adbc7b0a4cb3
jbelou3 <belousjeanluc@gmail.com>
2018-03-16 13:18:29 -0500

    Full Trash
    TRASH

---
+++ Assets/Scripts/Game Manager/UIManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+
+	public PlayerController playerController;
+	public Text jumpModeText;
+
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Alpha1)) {
+			changeJumpMode(1);
+		}
+		else if(Input.GetKeyDown(KeyCode.Alpha2)) {
+			changeJumpMode(2);
+		}
+		else if(Input.GetKeyDown(KeyCode.Alpha3)) {
+			changeJumpMode(3);
+		}
+	}
+
+	void changeJumpMode (int newMode) {
+		jumpModeText.text = "" + newMode;
+		playerController.setJumpMode(newMode);
+	}
+}
---
+++ Assets/Scripts/Terrain/ReboundProperties.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReboundProperties : MonoBehaviour {
+
+	public float reboundCoefficient = 1.0f;
+
+}
